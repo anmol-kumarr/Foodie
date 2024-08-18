9db11543fa9150f8c944ad1d6961d4620c52fc1d
Anmol kumar <anmolkumar31884@gmail.com>
2024-08-18 22:54:49 +0530

    route added and tailwind added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import routes from './routes/route';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <RouterProvider router={routes}></RouterProvider>
   );
 }
 
