6f3d71e080a7dd1e89f3c13fa25921a93eed3b0f
Anmol kumar <anmolkumar31884@gmail.com>
2024-08-19 19:39:10 +0530

    redux store added

---
+++ src/utils/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import locationSlice from "./locationSlice";
+
+const store=configureStore({
+    reducer:{
+        location:locationSlice
+    }
+})
+export default store
