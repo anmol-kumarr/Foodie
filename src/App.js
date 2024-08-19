c8900757efc22a43075685ba12b217b7e3636f33
Anmol kumar <anmolkumar31884@gmail.com>
2024-08-19 19:35:21 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import routes from './routes/route';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addLocation } from './utils/locationSlice';
 
 function App() {
+  const dispatch = useDispatch()
+  // const city = useSelector(store => store.location.city)
+  // useEffect(() => {
+  //   localStorage.setItem('user-coordinates', JSON.stringify(city))
+  // }, [])
+
+  // useEffect(() => {
+  //   const localCoordinates = localStorage.getItem('user-coordinates')
+  //   if (localCoordinates) {
+  //     const coordinates = JSON.parse(localCoordinates)
+  //     dispatch(addLocation(coordinates))
+  //   }
+  // },[])
+
   return (
     <RouterProvider router={routes}></RouterProvider>
   );
