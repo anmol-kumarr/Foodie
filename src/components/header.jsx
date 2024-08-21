c47b6371db9efc72ba4940dfaf5d949d57a093c5
Anmol kumar <anmolkumar31884@gmail.com>
2024-08-21 20:41:18 +0530

    header changed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,3 @@
-import logo from '../assets/logo.png'
 import { IoLocationOutline } from "react-icons/io5";
 import { IoSearch } from "react-icons/io5";
 import { FiShoppingCart } from "react-icons/fi";
@@ -115,83 +114,85 @@ const Header = () => {
     useEffect(() => {
         if (geoCity) {
 
-            const city = geoCity[0]?.name+','+geoCity[0]?.state
-            dispatch(addLocation({city,long:userCoordinates.long,lat:userCoordinates.lat}))
+            const city = geoCity[0]?.name + ',' + geoCity[0]?.state
+            dispatch(addLocation({ city, long: userCoordinates.long, lat: userCoordinates.lat }))
             // console.log(city)
         }
     }, [geoCity])
     return (
-        <header className='max-w-[1200px] mx-auto p-4  sm:bg-white flex justify-between items-center'>
+        <header className='shadow-md shadow-slate-200'>
 
+            <div className=" max-w-[1200px] mx-auto p-4  sm:bg-white flex justify-between items-center">
 
-            <div className='flex gap-4 items-center'>
+                <div className='flex gap-4 items-center'>
 
-                <h1 className='text-3xl font-extrabold text-pink-500  font-logo'>Foodie</h1>
+                    <h1 className='text-3xl font-extrabold text-pink-500  font-logo'>Foodie</h1>
 
-                {/* <div className=' relative '> */}
+                    {/* <div className=' relative '> */}
 
-                <div onClick={handleDivClick} className=' relative cursor-pointer text-sm flex items-center gap-1 group'  >
-                    <div className=''>
-                        <IoLocationOutline className='' />
-                    </div>
-                    <div className='text-text-gray text-[13px] font-medium font-gilory'>
-                        <p>Select location</p>
-                        <p className='text-[12px] font-semibold'>{myCity}</p>
-                    </div>
-
-                    <div ref={divRef} className={`border-solid border-[1px] overflow-hidden top-[56px] ${isVisible ? 'block' : 'hidden'} rounded-md w-[230px] z-10 bg-white absolute `}>
-                        <div onClick={currentLocationHandler} className='bg-red-400 rounded-t-md p-3 text-white flex items-center gap-1'>
-                            <IoLocationOutline className='' />
-                            Use current location
-                        </div>
-                        <div className={`${isVisible ? '' : 'rounded-b-md'} p-1 border-solid border-b-[1px] `}>
-                            <input onChange={(e) => setCityInput(e.target.value)} value={cityInput} className='border-none p-2  outline-none' type="text" placeholder='Enter your City' />
-                        </div>
+                    <div onClick={handleDivClick} className=' relative cursor-pointer text-sm flex items-center gap-1 group'  >
                         <div className=''>
-                            {
-                                CityInputIndia && CityInputIndia.length > 0 && CityInputIndia.map((item, index) => (
-                                    <div onClick={() => cityLocationHandler(item)} key={item?.properties?.place_id} className='text-text-gray font-gilory m-2'>
-                                        <p className='font-semibold'>{item?.properties?.address_line1}</p>
-                                        <p className='font-normal text-sm mb-1'>
-                                            {
-                                                item?.properties?.formatted.length > 27 ? <span>{item?.properties?.formatted.slice(0, 27)}...</span> : item?.properties?.formatted
-                                            }
-                                        </p>
-                                        {
-                                            CityInputIndia.length > index + 1 && <hr />
-                                        }
-                                    </div>
-                                ))
-                            }
+                            <IoLocationOutline className='' />
+                        </div>
+                        <div className='text-text-gray text-[13px] font-medium font-gilory'>
+                            <p>Select location</p>
+                            <p className='text-[12px] font-semibold'>{myCity}</p>
                         </div>
 
+                        <div ref={divRef} className={`border-solid border-[1px] overflow-hidden top-[56px] ${isVisible ? 'block' : 'hidden'} rounded-md w-[230px] z-10 bg-white absolute `}>
+                            <div onClick={currentLocationHandler} className='bg-red-400 rounded-t-md p-3 text-white flex items-center gap-1'>
+                                <IoLocationOutline className='' />
+                                Use current location
+                            </div>
+                            <div className={`${isVisible ? '' : 'rounded-b-md'} p-1 border-solid border-b-[1px] `}>
+                                <input onChange={(e) => setCityInput(e.target.value)} value={cityInput} className='border-none p-2  outline-none' type="text" placeholder='Enter your City' />
+                            </div>
+                            <div className=''>
+                                {
+                                    CityInputIndia && CityInputIndia.length > 0 && CityInputIndia.map((item, index) => (
+                                        <div onClick={() => cityLocationHandler(item)} key={item?.properties?.place_id} className='text-text-gray font-gilory m-2'>
+                                            <p className='font-semibold'>{item?.properties?.address_line1}</p>
+                                            <p className='font-normal text-sm mb-1'>
+                                                {
+                                                    item?.properties?.formatted.length > 27 ? <span>{item?.properties?.formatted.slice(0, 27)}...</span> : item?.properties?.formatted
+                                                }
+                                            </p>
+                                            {
+                                                CityInputIndia.length > index + 1 && <hr />
+                                            }
+                                        </div>
+                                    ))
+                                }
+                            </div>
+
+                        </div>
                     </div>
+
+                    {/* </div> */}
+
                 </div>
 
-                {/* </div> */}
 
-            </div>
+                <div>
+                    <div className='flex gap-6 items-center text-text-gray'>
+                        <div className='font-gilory text-sm flex items-center gap-1 bg-red-400 text-white px-4 py-2 rounded-md'>
+                            <BiSolidOffer />
+                            Offers
+                        </div>
 
+                        <div className='text-sm p-2 rounded-md w-[270px] flex items-center gap-2 border-solid border-[1px] border-gray-300'>
+                            <input className='border-none outline-none w-[90%] ' type="text" placeholder='Search for Resturants & foods' />
+                            <span className='text-lg'>
+                                <IoSearch></IoSearch>
+                            </span>
+                        </div>
 
-            <div>
-                <div className='flex gap-6 items-center text-text-gray'>
-                    <div className='font-gilory text-sm flex items-center gap-1 bg-red-400 text-white px-4 py-2 rounded-md'>
-                        <BiSolidOffer />
-                        Offers
-                    </div>
-
-                    <div className='text-sm p-2 rounded-md w-[270px] flex items-center gap-2 border-solid border-[1px] border-gray-300'>
-                        <input className='border-none outline-none w-[90%] ' type="text" placeholder='Search for Resturants & foods' />
-                        <span className='text-lg'>
-                            <IoSearch></IoSearch>
-                        </span>
-                    </div>
-
-                    <div className='text-xl'>
-                        <FiShoppingCart></FiShoppingCart>
-                    </div>
-                    <div className='text-xl'>
-                        <FaRegUser></FaRegUser>
+                        <div className='text-xl'>
+                            <FiShoppingCart></FiShoppingCart>
+                        </div>
+                        <div className='text-xl'>
+                            <FaRegUser></FaRegUser>
+                        </div>
                     </div>
                 </div>
             </div>
