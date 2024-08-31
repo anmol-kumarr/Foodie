9425b5d0d985f1af2753cb3c1f8cfde17a746a8e
Anmol kumar <anmolkumar31884@gmail.com>
2024-08-31 22:19:04 +0530

    trending components added

---
+++ src/components/home/trendingCarousel.jsx
@@ -0,0 +1,118 @@
+import { MdArrowBackIosNew, MdOutlineArrowForwardIos } from "react-icons/md";
+// import { ImageCard } from "./home/imgCard"
+import ReactSimplyCarousel from 'react-simply-carousel';
+import { useState } from "react";
+import { ShimmerThumbnail } from "react-shimmer-effects";
+import Card from "../card";
+const TrendingCarouselDiv = ({ data }) => {
+
+    const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+
+    return (
+        <div className="max-w-[1300px] mx-auto relative">
+
+            <ReactSimplyCarousel
+                activeSlideIndex={activeSlideIndex}
+                onRequestChange={setActiveSlideIndex}
+                itemsToShow={4}
+                itemsToScroll={1}
+                infinite={true}
+
+                containerProps={
+                    {
+                        style: {
+                            gap: '20px'
+                        }
+                    }
+                }
+                forwardBtnProps={{
+
+                    style: {
+                        position: "absolute",
+                        right: '-1%',
+                        bottom: '50%',
+                        top: '50%',
+                        alignSelf: 'center',
+                        zIndex: '10',
+                        // alignSelf: 'center',
+                        boxShadow: '0px 0px 30px black',
+                        background: 'white',
+                        border: 'none',
+                        color: 'rgba(2, 6, 12, 0.75)',
+                        cursor: 'pointer',
+                        fontSize: '1.03rem',
+                        borderRadius: '50%',
+                        // height:20,
+                        lineHeight: 1,
+                        textAlign: 'center',
+
+                        padding: '5px',
+
+                    },
+                    children: <span><MdOutlineArrowForwardIos></MdOutlineArrowForwardIos></span>,
+                }}
+                backwardBtnProps={{
+
+                    style: {
+                        position: "absolute",
+                        left: '-4px',
+                        bottom: '50%',
+                        top: '50%',
+                        zIndex: '10',
+                        alignSelf: 'center',
+                        boxShadow: '0px 0px 30px black',
+                        background: 'white',
+                        border: 'none',
+                        color: 'rgba(2, 6, 12, 0.75)',
+                        cursor: 'pointer',
+                        fontSize: '1.03rem',
+                        borderRadius: '50%',
+                        // height:20,
+                        lineHeight: 1,
+                        textAlign: 'center',
+
+                        padding: '5px',
+
+                    },
+                    children: <span><MdArrowBackIosNew></MdArrowBackIosNew></span>,
+                }}
+                responsiveProps={[
+                    {
+
+                        itemsToShow:4,
+                        itemsToScroll: 1,
+                        minWidth: 900,
+
+                    },
+                ]}
+                speed={400}
+                easing="linear"
+
+            >
+
+                {data ? data && data.map((item) => (
+                    <Card key={item.info.id} item={item}></Card>
+                )) :
+                    <div className="flex gap-4 p-2 ">
+                        <div className="h-[148px] w-[158px] rounded-md">
+                            <ShimmerThumbnail height={305} width={288} />
+                        </div>
+                    
+                    
+
+
+                    </div>
+
+                }
+
+
+
+
+
+            </ReactSimplyCarousel>
+
+
+        </div>
+    )
+}
+export default TrendingCarouselDiv
