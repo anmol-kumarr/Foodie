9d67547ece72d2c924740b3af47f8416f5a910bd
Anmol kumar <anmolkumar31884@gmail.com>
2024-09-03 13:31:54 +0530

    footer added

---
+++ src/components/footer.jsx
@@ -0,0 +1,103 @@
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa6";
+
+function Footer() {
+    return (
+        <div className="bg-black text-gray-100 list-none font-gilory">
+            <div className="max-w-[1100px] p-5 mx-auto flex flex-wrap gap-2 justify-between">
+                <div className="footer-heading">
+                    <div className="footer-logo-cont">
+
+
+                        <span className="font-logo font-extrabold text-4xl">Foodie</span>
+                    </div>
+                    <p className="font-light text-sm">
+
+                        © 2024 Bundl Technologies Pvt. Ltd
+                    </p>
+
+
+                    <div className="flex text-white text-xl gap-3 p-2">
+                        <FaFacebookF />
+                        <FaInstagram />
+                        <FaTwitter />
+                        <FaYoutube />
+
+                    </div>
+                </div>
+
+
+                <div className="">
+                    <p className="font-semibold mb-1">
+                        Company
+                    </p>
+                    <div className="text-sm font-light flex flex-col gap-1 p-2 cursor-pointer">
+
+                        <li>
+                            About
+
+                        </li>
+                        <li>
+                            Careers
+
+                        </li>
+                        <li>
+                            Team
+
+                        </li>
+                        <li>
+                            Swiggy One
+
+                        </li>
+                        <li>
+
+                            Swiggy Instamart
+                        </li>
+                        <li>
+
+                            Swiggy Genie
+                        </li>
+                    </div>
+                </div>
+                <div className="">
+                    <div>
+
+                        <p className="font-semibold mb-1 ">
+                            Contact us
+                        </p>
+                        <div className="text-sm font-light flex flex-col gap-1 p-2 cursor-pointer">
+
+                            <li>Help & Support</li>
+                            <li>Partner with us</li>
+                            <li>Ride with us</li>
+                        </div>
+
+                    </div>
+                    <div>
+                        <p className="font-semibold mb-1 mt-3">Legal</p>
+                        <div className="text-sm font-light flex flex-col gap-1 p-2 cursor-pointer">
+
+                            <li>Terms & conditions</li>
+                            <li>Cookie Policy</li>
+                            <li>Privacy Policy</li>
+                            <li>Investor Relations</li>
+                        </div>
+                    </div>
+                </div>
+                <div className="">
+                    <p className="font-semibold mb-1">We deliver to:</p>
+                    <div className="text-sm font-light flex flex-col gap-1 p-2 cursor-pointer">
+
+                        <li>Banglore</li>
+                        <li>Gurgaon</li>
+                        <li>Hydrabad</li>
+                        <li>Delhi</li>
+                        <li>Mumbai</li>
+                        <li>Pune</li>
+                        <li>589+ Cities</li>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer;
